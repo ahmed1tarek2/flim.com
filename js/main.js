1e1174fcff135e2d7d02aa365bea9d2cf64935bb
ahmed1tarek2 <ahmedbatika123@gmail.com>
2025-08-28 18:41:10 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
-const menuIcon = document.getElementById("menu-icon");
-const hiddenList = document.getElementById("hidden-list");
-const header = document.getElementById("header");
-const homeLogo = document.getElementById("home-logo");
+window.addEventListener("DOMContentLoaded", () => {
+  const menuIcon = document.getElementById("menu-icon");
+  const hiddenList = document.getElementById("hidden-list");
+  const header = document.getElementById("header");
+  const homeLogo = document.getElementById("home-logo");
 
-menuIcon.addEventListener("click", () => {
+  menuIcon.addEventListener("click", () => {
     hiddenList.classList.toggle("hidden");
-});
+  });
 
-window.onscroll = () => {
-  hiddenList.classList.toggle("hidden",window.screen.width<=320);
-}
+  window.addEventListener("load", () => {
+    hiddenList.classList.add("hidden");
+  });
+  
+  window.onscroll = () => {
+    hiddenList.classList.toggle("hidden", window.screen.width <= 360);
+  };
 
-window.addEventListener('scroll', () => {
+  window.addEventListener("scroll", () => {
     header.classList.toggle("bg-[var(--bg-color)]", window.scrollY > 0);
     menuIcon.classList.toggle("text-[var(--text-color)]", window.scrollY > 0);
     homeLogo.classList.toggle("text-[var(--text-color)]", window.scrollY > 0);
+  });
 });
